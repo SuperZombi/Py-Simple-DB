9f6c4fa536f814b77fea2954c0758339bf7f4d72
Super Zombi <rost201541@gmail.com>
2022-06-26 20:31:38 +0300

    Add files via upload

---
+++ examples/simple_db.py
@@ -0,0 +1,55 @@
+from DataBase import DataBase
+tracks = DataBase("database/tracks.bd")
+
+
+# ADD
+def add():
+	tracks.add(track=request.json['track_name'],
+				artist=request.json['artist'],
+				genre=request.json['genre'],
+				date=request.json['date'])
+
+
+# GET
+def get():
+	track_id = tracks.get(artist=request.json['artist'], track=request.json['track_name'])
+	track = tracks.get_by_id(track_id)
+	return track
+
+
+# GET ALL artist tracks
+def get_all():
+	array = tracks.get(artist=request.json['artist'], all=True)
+	all_tracks = []
+	for i in array:
+		track = tracks.get_by_id(i)
+		all_tracks.append(track)
+	return all_tracks
+
+
+# GET ALL DATA
+def get_all_data():
+	all_tracks = []
+	for i in tracks.data:
+		all_tracks.append(i)
+	return all_tracks
+
+
+# DELETE
+def delete():
+	track_id = tracks.get(artist=request.json['artist'], track=request.json['track_name'])
+	tracks.delete(track_id)
+
+
+# EDIT
+def edit():
+	track_id = tracks.get(artist=request.json['artist'], track=request.json['track_name'])
+	track = tracks.get_by_id(track_id)
+
+	track['genre'] = request.json['genre']
+	tracks.save()
+
+
+# Check if already exists
+if tracks.get(artist=request.form['artist'], track=request.form['track_name']):
+	return "track already exists"
---
+++ examples/unique_db.py
@@ -0,0 +1,51 @@
+from DataBase import DataBase
+users = DataBase("database/users.bd", unique="user")
+
+
+# ADD
+def add():
+	users.add(  user=request.json['user_name'],
+				password=request.json['password'],
+				registration_time=time.time(),
+				role="admin")
+
+
+# GET
+def get():
+	user = users.get_by_id(request.json['user_name'])
+	return user
+
+
+# Find
+def find():
+	user_id = users.get(role="admin")
+	user = users.get_by_id(user_id)
+
+
+# Find ALL
+def find():
+	array = users.get(role="admin", all=True)
+	for i in array:
+		user = users.get_by_id(i)
+
+
+# GET ALL DATA
+def get_all_data():
+	return users.data
+
+
+# DELETE
+def delete():
+	users.delete(request.json['user_name'])
+
+
+# EDIT
+def edit():
+	user = users.get_by_id(request.json['user_name'])
+	user['role'] = 'admin'
+	users.save()
+
+
+# Check if already exists
+if users.get_by_id(request.json['user_name']):
+	return "user already exists"
